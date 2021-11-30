9d7953bad17eb0cdc71e5d735d9e772058c41add
benrutter <benrutter@posteo.net>
2021-11-30 20:24:08 +0000

    refactor

--- rad-waves/surfer.js
+++ rad-waves/surfer.js
@@ -9,13 +9,16 @@ class Surfer {
     this.spinning = 0;
     this.maxSpinning = 5;
     this.floater = false;
-    this.maxSpeed = 1000;
     this.waveHeight = wave.waveHeight;
     this.speed = 700;
+    this.maxSpeed = 2000;
     this.weight = 15;
+    this.boost = 20;
     this.jump = 500;
-    this.stall = 1000;
-    this.waveSpeed = 200;
+    this.state = 'norm';
+    this.stall = 10;
+    this.minSpeed = 100;
+    this.waveSpeed = 0.4;
     this.airCounter = 0;
     this.createAnims();
     this.setControls();
@@ -43,20 +46,28 @@ class Surfer {
       this.spinning = Math.max(this.spinning, -this.maxSpinning);
     });
     // jump
+    this.context.input.keyboard.on('keydown-A', () => {
+      this.state = 'boost';
+      console.log('woo');
+
+    });
     this.context.input.keyboard.on('keyup-A', () => {
-      this.jumpCounter = 50;
-      this.speed += this.jump;
+      this.state = 'norm';
     });
     // stall
     this.context.input.keyboard.on('keydown-S', () => {
-      // this isn't going to do much until speed mechanics are more developed
+      this.state = 'stall';
       this.speed -= this.stall;
+      this.speed = Math.max(this.speed, this.minSpeed);
+    });
+    this.context.input.keyboard.on('keyup-S', () => {
+      this.state = 'norm';
     });
   }
 
   draw() {
     this.sprite = this.context.physics.add.sprite(
-      500,
+      1500,
       500,
       this.surferSprite
     ).setScale(2).setCollideWorldBounds(true);
@@ -83,20 +94,21 @@ class Surfer {
 
 
   updateMovementOnWave() {
-
-    if (this.jumpCounter <= 0) {
-      // for now, just move the speed closer and closer to max speed
-      this.speed = (this.speed + this.maxSpeed) / 2;
-      this.speed = Math.max(this.speed, this.maxSpeed);
+    if (this.state == 'boost') {
+      this.speed += this.boost;
+      this.speed = Math.min(this.speed, this.maxSpeed);
+    } else if (this.state == 'stall') {
+      this.speed -= this.stall;
+      this.speed = Math.max(this.speed, 0);
     }
+    this.speed *= 0.99;
     let angle = this.spin * Math.PI / 180;
-    this.sprite.setVelocityX((this.speed * Math.cos(angle))-this.waveSpeed);
+    this.sprite.setVelocityX((this.speed * Math.cos(angle))-this.waveSpeed*(this.sprite.body.x+this.sprite.body.y));
     //console.log(this.sprite.body.velocity.x)
     this.sprite.setVelocityY(-this.speed * Math.sin(angle));
   }
 
   updateMovementOffWave() {
-
     this.sprite.setVelocityY(this.sprite.body.velocity.y + (this.weight * (this.airCounter/5)));
   }
 
